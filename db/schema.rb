f97a9d0d2c2bb191d499bb7fd96275cba9a2292a
achrysaetos <achrysaetos@yahoo.com>
2020-11-23 17:53:54 -0600

    bin/rails generate model Comment author_name:string body:text tweet:references, bin/rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_19_033920) do
+ActiveRecord::Schema.define(version: 2020_11_23_235214) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "author_name"
+    t.text "body"
+    t.integer "tweet_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["tweet_id"], name: "index_comments_on_tweet_id"
+  end
 
   create_table "tweets", force: :cascade do |t|
     t.text "tweet"
@@ -34,4 +43,5 @@ ActiveRecord::Schema.define(version: 2020_11_19_033920) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "comments", "tweets"
 end
